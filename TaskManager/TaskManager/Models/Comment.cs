ac695ea4c4a90f7b3b6aed7d1020c861f715c05f
giobiba <g.blanar82@gmail.com>
2020-11-21 12:06:01 +0200

    Adaugat fisierele vechi
    Ramane de rezolvat cu appcontextul, si de adaugat toate tabelele la baza de date

---
+++ TaskManager/TaskManager/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int id_com { get; set; }
+        public int id_us { get; set; }
+        public int id_tsk { get; set; }
+        public string Content { get; set; }
+
+        public virtual Task Task { get; set; }
+    }
+}
