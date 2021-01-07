da532abe5fecbaa8a832e7095b356366c0459943
Alin <34008360+Alin@users.noreply.github.com>
2021-01-07 20:18:30 +0200

    Nu e de fapt design este errorhandling

--- TaskManager/TaskManager/App_Start/FilterConfig.cs
+++ TaskManager/TaskManager/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace TaskManager
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
